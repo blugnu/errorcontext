ff67c555ed7d51de8f2d88560f6773e0f6145f48
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2024-04-26 11:29:59 +1200

    feat: errors.Join() function introduced and errors.Wrap() simplified (fixes #8)

--- factories.go
+++ factories.go
@@ -6,12 +6,7 @@ import (
 	"fmt"
 )
 
-// New creates a new error with the supplied Context.
-// The error wraps a new error created by passing the
-// specified string to `errors.New()`.
-func New(ctx context.Context, s string) error {
-	return ErrorWithContext{ctx, errors.New(s)}
-}
+var ErrIllegalOperation = errors.New("illegal operation")
 
 // Errorf creates a new error with the supplied Context.
 // The error wraps an error created by passing a supplied
@@ -23,22 +18,41 @@ func Errorf(ctx context.Context, format string, args ...interface{}) error {
 	return ErrorWithContext{ctx, fmt.Errorf(format, args...)}
 }
 
-// Wrap creates a new error wrapping at least one error with a
-// specified context.  However many arguments are specified,
-// if all errors are nil then the function returns nil.
+// Join creates a new error wrapping any number of supplied errors
+// with a specified context.
+//
+// If all errors are nil then the function returns nil.
+func Join(ctx context.Context, errs ...error) error {
+	if err := errors.Join(errs...); err != nil {
+		return ErrorWithContext{ctx, err}
+	}
+	return nil
+}
+
+// New creates a new error with the supplied Context.
+// The error wraps a new error created by passing the
+// specified string to `errors.New()`.
+func New(ctx context.Context, s string) error {
+	return ErrorWithContext{ctx, errors.New(s)}
+}
+
+// Wrap creates a new error wrapping one or two errors with a
+// specified context.  If both errors are nil then the function
+// returns nil.
 //
 // The error returned by the function wraps the specified error(s)
 // differently, depending on the number of non-nil errors specified.
 //
-// * 1 Error
+// # Called with 1 Error
 //
-// If only one non-nil error is specified, it is wrapped directly.
+// If only one non-nil error is specified it is wrapped directly.
 //
-// * 2 Errors
+// # Called with 2 Errors
 //
-// If two errors are specified and both are not nil, then the first
-// is wrapped formatted to indicate the second as the cause.  i.e. both
-// of the following are equivalent statements when a and b are non-nil:
+// If two errors (a, b) are specified and both are not nil, then both
+// errors are wrapped within a new error of the form "a: b".
+//
+// i.e. the following are equivalent statements when a and b are non-nil:
 //
 //	errorcontext.Wrap(ctx, a, b)
 //	errorcontext.Wrap(ctx, fmt.Errorf("%w: %w", a, b))
@@ -46,38 +60,34 @@ func Errorf(ctx context.Context, format string, args ...interface{}) error {
 // If either a or b are nil (but not both), the non-nil error is
 // wrapped directly.
 //
-// * 3 or More
+// # Called with > 2 errors
 //
-// If three or more errors are specified, the function is
-// equivalent to:
-//
-//	errorcontext.Wrap(ctx, errors.Join(errs...))
-//
-// Unless all errors are nil, in which case nil is returned.
+// The function will panic with ErrIllegalOperation if called
+// with 3 or more error arguments, whether or not any or all of
+// those arguments are nil.  To wrap three or more errors use
+// the errorcontext.Join function.
 func Wrap(ctx context.Context, err error, errs ...error) error {
-	switch len(errs) {
-	case 0:
-		if err == nil {
-			return nil
-		}
-		return ErrorWithContext{ctx, err}
-
+	switch 1 + len(errs) {
 	case 1:
-		if err == nil && errs[0] == nil {
-			return nil
-		}
-		if err != nil && errs[0] != nil {
-			return ErrorWithContext{ctx, fmt.Errorf("%w: %w", err, errs[0])}
-		}
-		if err == nil {
-			err = errs[0]
-		}
-		return ErrorWithContext{ctx, err}
-
-	default:
-		if err = errors.Join(append([]error{err}, errs...)...); err != nil {
+		if err != nil {
 			return ErrorWithContext{ctx, err}
 		}
-		return nil
+
+	case 2:
+		switch {
+		case err == nil && errs[0] != nil:
+			return ErrorWithContext{ctx, errs[0]}
+
+		case err != nil && errs[0] == nil:
+			return ErrorWithContext{ctx, err}
+
+		case err != nil && errs[0] != nil:
+			return ErrorWithContext{ctx, fmt.Errorf("%w: %w", err, errs[0])}
+		}
+
+	default:
+		panic(fmt.Errorf("errorcontext.Wrap: %w: may only be called with 1 or 2 errors; "+
+			"use errorcontext.Join for > 2 error arguments", ErrIllegalOperation))
 	}
+	return nil
 }
--- factories_test.go
+++ factories_test.go
@@ -4,112 +4,162 @@ import (
 	"context"
 	"errors"
 	"testing"
+
+	"github.com/blugnu/test"
 )
 
-func assert(t *testing.T, sut ErrorWithContext, wantCtx context.Context, errorIsWanted func() bool, wantString string) {
-	t.Run("wraps context", func(t *testing.T) {
-		wanted := wantCtx
-		got := sut.ctx
-		if wanted != got {
-			t.Errorf("\nwanted %#v\ngot    %#v", wanted, got)
-		}
-	})
-
-	t.Run("wraps error", func(t *testing.T) {
-		wanted := true
-		got := errorIsWanted()
-		if wanted != got {
-			t.Errorf("\nwanted %#v\ngot    %#v", wanted, got)
-		}
-	})
-
-	t.Run("Error()", func(t *testing.T) {
-		wanted := wantString
-		got := sut.Error()
-		if wanted != got {
-			t.Errorf("\nwanted %#v\ngot    %#v", wanted, got)
-		}
-	})
-}
-
-func Test_New(t *testing.T) {
+func TestFactories(t *testing.T) {
 	// ARRANGE
 	ctx := context.Background()
+	a := errors.New("a")
+	b := errors.New("b")
 
-	// ACT
-	sut := New(ctx, "some new error").(ErrorWithContext)
-
-	// ASSERT
-	assert(t, sut, ctx, func() bool { return sut.error != nil }, "some new error")
-}
-
-func Test_Errorf(t *testing.T) {
-	// ARRANGE
-	ctx := context.Background()
-	err := errors.New("some error")
-
-	// ACT
-	sut := Errorf(ctx, "narrative: %w", err).(ErrorWithContext)
-
-	// ASSERT
-	assert(t, sut, ctx, func() bool { return errors.Is(sut, err) }, "narrative: some error")
-}
-
-func Test_Wrap(t *testing.T) {
-	// ARRANGE
-	ctx := context.Background()
-	w := errors.New("wrapped")
-	c := errors.New("cause")
-	x := errors.New("extra")
-
-	type result struct {
-		string
-	}
 	testcases := []struct {
-		name string
-		args []error
-		*result
+		scenario string
+		exec     func(t *testing.T)
 	}{
-		{name: "all nil (1)", args: []error{nil}, result: nil},
-		{name: "all nil (2)", args: []error{nil, nil}, result: nil},
-		{name: "all nil (3)", args: []error{nil, nil, nil}, result: nil},
-		{name: "one non-nil (1)", args: []error{w}, result: &result{string: "wrapped"}},
-		{name: "1st of 2 non-nil", args: []error{w, nil}, result: &result{string: "wrapped"}},
-		{name: "2nd of 2 non-nil", args: []error{nil, w}, result: &result{string: "wrapped"}},
-		{name: "both of 2 non-nil", args: []error{w, c}, result: &result{string: "wrapped: cause"}},
-		{name: "1st of 3 non-nil", args: []error{w, nil, nil}, result: &result{string: "wrapped"}},
-		{name: "2nd of 3 non-nil", args: []error{nil, w, nil}, result: &result{string: "wrapped"}},
-		{name: "3rd of 3 non-nil", args: []error{nil, nil, w}, result: &result{string: "wrapped"}},
-		{name: "1st and 2nd of 3 non-nil", args: []error{w, c, nil}, result: &result{string: "wrapped\ncause"}},
-		{name: "1st and 3rd of 3 non-nil", args: []error{w, nil, c}, result: &result{string: "wrapped\ncause"}},
-		{name: "2nd and 3rd of 3 non-nil", args: []error{nil, w, c}, result: &result{string: "wrapped\ncause"}},
-		{name: "all of 3 non-nil", args: []error{w, c, x}, result: &result{string: "wrapped\ncause\nextra"}},
+		// Errorf tests
+		{scenario: "Errorf(\"narrative: %w\",a)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Errorf(ctx, "narrative: %w", a)
+
+				// ASSERT
+				test.IsTrue(t, result != nil)
+				test.That(t, result.Error()).Equals("narrative: a")
+				if result, ok := test.IsType[ErrorWithContext](t, result); ok {
+					test.Error(t, result).Is(a)
+				}
+			},
+		},
+
+		// Join tests
+		{scenario: "Join(nil)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Join(ctx, nil)
+
+				// ASSERT
+				test.IsNil(t, result)
+			},
+		},
+		{scenario: "Join(a)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Join(ctx, a)
+
+				// ASSERT
+				test.IsTrue(t, result != nil)
+				test.That(t, result.Error()).Equals("a")
+				if result, ok := test.IsType[ErrorWithContext](t, result); ok {
+					test.Error(t, result).Is(a)
+				}
+			},
+		},
+		{scenario: "Join(a,b)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Join(ctx, a, b)
+
+				// ASSERT
+				test.IsTrue(t, result != nil)
+				test.That(t, result.Error()).Equals("a\nb")
+				if result, ok := test.IsType[ErrorWithContext](t, result); ok {
+					test.Error(t, result).Is(a)
+					test.Error(t, result).Is(b)
+				}
+			},
+		},
+
+		// New tests
+		{scenario: "New(ctx, \"a\")",
+			exec: func(t *testing.T) {
+				// ACT
+				result := New(ctx, "a")
+
+				// ASSERT
+				test.IsTrue(t, result != nil)
+				test.That(t, result.Error()).Equals("a")
+				_, _ = test.IsType[ErrorWithContext](t, result)
+			},
+		},
+
+		// Wrap tests
+		{scenario: "Wrap(nil)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Wrap(ctx, nil)
+
+				// ASSERT
+				test.IsNil(t, result)
+			},
+		},
+		{scenario: "Wrap(a)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Wrap(ctx, a)
+
+				// ASSERT
+				test.IsTrue(t, result != nil)
+				test.That(t, result.Error()).Equals("a")
+				if result, ok := test.IsType[ErrorWithContext](t, result); ok {
+					test.Error(t, result).Is(a)
+				}
+			},
+		},
+		{scenario: "Wrap(a, nil)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Wrap(ctx, a, nil)
+
+				// ASSERT
+				test.IsTrue(t, result != nil)
+				test.That(t, result.Error()).Equals("a")
+				if result, ok := test.IsType[ErrorWithContext](t, result); ok {
+					test.Error(t, result).Is(a)
+				}
+			},
+		},
+		{scenario: "Wrap(nil,b)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Wrap(ctx, nil, b)
+
+				// ASSERT
+				test.IsTrue(t, result != nil)
+				test.That(t, result.Error()).Equals("b")
+				if result, ok := test.IsType[ErrorWithContext](t, result); ok {
+					test.Error(t, result).Is(b)
+				}
+			},
+		},
+		{scenario: "Wrap(a,b)",
+			exec: func(t *testing.T) {
+				// ACT
+				result := Wrap(ctx, a, b)
+
+				// ASSERT
+				test.IsTrue(t, result != nil)
+				test.That(t, result.Error()).Equals("a: b")
+				if result, ok := test.IsType[ErrorWithContext](t, result); ok {
+					test.Error(t, result).Is(a)
+					test.Error(t, result).Is(b)
+				}
+			},
+		},
+		{scenario: "Wrap(nil,nil,nil)",
+			exec: func(t *testing.T) {
+				// ARRANGE ASSERT
+				defer test.ExpectPanic(ErrIllegalOperation).Assert(t)
+
+				// ACT
+				_ = Wrap(ctx, nil, nil, nil)
+			},
+		},
 	}
 	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			// ACT
-			sut := Wrap(ctx, tc.args[0], tc.args[1:]...)
-
-			// ASSERT
-			switch {
-			case tc.result == nil && sut == nil:
-				return
-			case tc.result == nil && sut != nil:
-				t.Errorf("expected nil")
-			case tc.result != nil && sut == nil:
-				t.Errorf("expected error")
-			default:
-				for _, err := range tc.args {
-					if err != nil && !errors.Is(sut, err) {
-						t.Errorf("expected error to wrap: %v", err)
-					}
-				}
-				wanted := tc.result.string
-				got := sut.Error()
-				if wanted != got {
-					t.Errorf("\nwanted %#v\ngot    %#v", wanted, got)
-				}
-			}
+		t.Run(tc.scenario, func(t *testing.T) {
+			tc.exec(t)
 		})
 	}
 }
